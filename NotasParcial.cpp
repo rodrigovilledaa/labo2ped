b471a565ad6332b5bd295ba87b7cf5c0052f1bd8
BlueSwarly <00067425@uca.edu.sv>
2026-01-16 14:58:17 -0600

    Agregar_Estudiantes

--- NotasParcial.cpp
+++ NotasParcial.cpp
@@ -8,18 +8,38 @@ struct Estudiante{
 };
 
 struct nodo{
-    int dato;
+    Estudiante dato;
     nodo *sig;
 };
 
 class ListaEnlazada{
     private:
+
     nodo *head;
 
     public:
     ListaEnlazada();
-    void AgregarEstudiante(void);
-    void MostrarPromedio(void);
+    void AgregarEstudiante(string nombre, float NotaFinal);
+    float MostrarPromedio(void);
     void MostrarEstudiante(void);
 
 };
+
+ListaEnlazada::ListaEnlazada(){
+    head = nullptr;
+}
+
+void ListaEnlazada::AgregarEstudiante(string nombre, float NotaFinal){
+    nodo* nuevo = new nodo();
+    nuevo -> dato.nombre = nombre;
+    nuevo -> dato.NotaFinal = NotaFinal;
+    nuevo -> sig = head;
+    head = nuevo;
+
+    cout << "Nombre del Estudiante: ";
+    cin >> nombre;
+    cin.ignore();
+    cout << "Nota Final: ";
+    cin >> NotaFinal;
+
+}
